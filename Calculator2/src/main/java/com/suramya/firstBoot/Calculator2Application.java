ce66b8120783356201931e564a8806fb7568f8f2
DELL <DELL@DESKTOP-S32OCEI>
2024-06-25 10:00:24 +0530

    Second version

--- Calculator2/src/main/java/com/suramya/firstBoot/Calculator2Application.java
+++ Calculator2/src/main/java/com/suramya/firstBoot/Calculator2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Calculator2Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(Calculator2Application.class, args);
 	}
 
---
+++ Calculator2/src/main/java/com/suramya/firstBoot/service/Myservice.java
@@ -0,0 +1,7 @@
+package com.suramya.firstBoot.service;
+
+public class Myservice {
+	public void show() {
+		System.out.println("hi");
+	}
+}
